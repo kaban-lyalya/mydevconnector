986537fb39b20671ed4f844209d002886d6532f1
kaban-lyalya <kabanlyalya@mail.ru>
2018-05-09 16:03:36 +0300

    fixit jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -81,12 +81,16 @@ router.post("/login", (req, res) => {
 // @route  GET api/users/current
 // @desc   Return current user
 // @access Private
-router.get('/current', passport.authenticate('jwt', { session: false), (req, res) => {
-  res.json({
-    id: req.user.id,
-    name: req.user.name,
-    email: req.user.email
-  });
-});
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
